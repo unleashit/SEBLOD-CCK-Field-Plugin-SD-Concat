446b6071564e5a28c5ad63d95d800d3105913ad8
Simon Dowdles <simon@onestuckpixel.com>
2012-02-07 15:10:36 +0200

    Updating to Version 2.1.2

--- sd_field_concat.php
+++ sd_field_concat.php
@@ -1,11 +1,17 @@
 <?php
+/**
+* @version 			Seblod 2.0 More $Revision: 147 $
+* @package			Seblod (CCK for Joomla)
+* @author       	http://www.seblod.com
+* @copyright		Copyright (C) 2011 SEBLOD. All Rights Reserved.
+* @license 			GNU General Public License version 2 or later; see _LICENSE.php
+**/
+
 // No Direct Access
 defined( '_JEXEC' ) or die;
 
 jimport( 'joomla.plugin.plugin' );
 jimport( 'cck.construction.field.generic' );
-jimport( 'cck.rendering.rendering' );
-//jimport( 'cck.development.field' );
 
 // Plugin Class
 class plgCCK_FieldSd_Field_Concat extends plgCCK_FieldGeneric
@@ -24,15 +30,24 @@ class plgCCK_FieldSd_Field_Concat extends plgCCK_FieldGeneric
 		parent::g_onCCK_FieldConstruct( $data );
 	}
 	
-
-	// onCCK_FieldPrepareForm
-	public function onCCK_FieldPrepareForm( &$field, $value = '', &$config = array(), $inherit = array(), $return = false )
-	{	
-		
+	// onCCK_FieldPrepareContent
+	public function onCCK_FieldPrepareContent( &$field, $value = '', &$config = array() )
+	{
 		if ( self::$type != $field->type ) {
 			return;
 		}
-
+		parent::g_onCCK_FieldPrepareContent( $field, $config );
+		
+		$field->value	=	$value;
+	}
+	
+	// onCCK_FieldPrepareForm
+	public function onCCK_FieldPrepareForm( &$field, $value = '', &$config = array(), $inherit = array(), $return = false )
+	{		
+		if ( self::$type != $field->type ) {
+			return;
+		}
+		self::$path	=	parent::g_getPath( self::$type.'/' );
 		parent::g_onCCK_FieldPrepareForm( $field, $config );
 		
 		// Init
@@ -45,10 +60,8 @@ class plgCCK_FieldSd_Field_Concat extends plgCCK_FieldGeneric
 		}
 		$value		=	( $value != '' ) ? $value : $field->defaultvalue;
 		$value		=	( $value != ' ' ) ? $value : '';
-		$value		=	htmlspecialchars( $value );
+		$value		=	htmlspecialchars( $value, ENT_QUOTES );
 		
-		$options2	=	CCK_Dev::fromJSON( $field->options2 );
-				
 		// Validate
 		$validate	=	'';
 		if ( $config['doValidation'] > 1 ) {
@@ -61,7 +74,9 @@ class plgCCK_FieldSd_Field_Concat extends plgCCK_FieldGeneric
 		}
 		
 		// Prepare
-		
+		//$class	=	'textarea text'.$validate.' '.$field->css;
+		//$form	=	'<input style="padding:10px;border:1px dotted #ccc;color:#666;width:250px;" id="'.$id.'" value="'.$value.'" />';
+		$form = '';
 		// Set
 		if ( ! $field->variation ) {
 			$field->form	=	$form;
@@ -72,7 +87,7 @@ class plgCCK_FieldSd_Field_Concat extends plgCCK_FieldGeneric
 			parent::g_getDisplayVariation( $field, $field->variation, $value, $value, $form, $id, $name, '<input' );
 		}
 		$field->value	=	$value;
-		
+
 		// Return
 		if ( $return === true ) {
 			return $field;
@@ -93,9 +108,15 @@ class plgCCK_FieldSd_Field_Concat extends plgCCK_FieldGeneric
 		$user = JFactory::getUser();
 		$options2 = CCK_Dev::fromJSON( $field->options2 );
 		$sdFieldList = @$options2['sd_core_field_concat_list'];
+		$SdJtext = @$options2['sd_core_field_concat_enable_jtext'];
+		$SdUserDateFormat = @$options2['sd_core_field_concat_date_format'];
+		$SdDateFormat = ($SdUserDateFormat !== '' ? $SdUserDateFormat : 'Y-m-d');
 		$sdFieldSeparator = (@$options2['sd_core_field_concat_separator'] > '' ? @$options2['sd_core_field_concat_separator'] : ' ');
-		$sdFieldList = ( preg_match("^[,]^",$sdFieldList) ? explode(",",$sdFieldList) : $sdFieldList );
+		$sdFieldSeparator = ($sdFieldSeparator == 'ns' ? '' : $sdFieldSeparator);
+		$sdFieldList = ( preg_match("^[||]^",$sdFieldList) ? explode("||",$sdFieldList) : $sdFieldList );
 		
+		$sdFieldTextValue = (@$options2['sd_core_field_concat_text_value'] > '' ? @$options2['sd_core_field_concat_text_value'] : 'v');
+
 		if(is_array($sdFieldList)):
 			
 			$sdConcatValue = '';
@@ -105,24 +126,250 @@ class plgCCK_FieldSd_Field_Concat extends plgCCK_FieldGeneric
 			foreach($sdFieldList as $sdField):
 				$sdNumStep++;
 				$sdField = trim($sdField);
-				if(preg_match("^[\[\]]^", $sdField)):
+				
+				if(preg_match("^<(.*?)>^", $sdField, $matches)):
+				$aitable = $matches[1];
+				/* Next Auto Increment */
+				$sebid = $config['pk'];
+				if(!$sebid > 0){	// Are we creating a new article? Yes
+				echo "we are add new, ";
+					$inifile = dirname(__FILE__).DS.'increments.ini';
+					if(JFile::exists($inifile)){
+						chmod($inifile, 0644);
+							$ini = parse_ini_file($inifile);
+							$db = JFactory::getDbo();
+							$pfx = $db->getPrefix();
+							// get auto increment of users desired table
+							$db->setQuery("SHOW TABLE STATUS LIKE '".$pfx.$aitable."'");
+							$db->query();
+							$dbresult = $db->loadAssoc();
+							$ai = $dbresult['Auto_increment'];
+							// get auto increment of content table for this ID
+							$db->setQuery("SHOW TABLE STATUS LIKE '".$pfx."content'");
+							$db->query();
+							$dbresult = $db->loadAssoc();
+							$artid = $dbresult['Auto_increment'];
+							$ini[$artid] = $ai;
+							$fopen = fopen($inifile, 'w');
+							$ini_in = "";
+							foreach($ini as $key=>$val){
+								$ini_in .= $key."=".$val."\r\n";
+							}
+							fwrite($fopen, $ini_in);
+							fclose($fopen);						
+					};
+				}else{ // We are updating an article
+					if($aitable > ''){
+						$sebid = $config['pk'];
+						$db = JFactory::getDbo();
+						$pfx = $db->getPrefix();
+						$db->setQuery("SHOW TABLE STATUS LIKE '".$pfx.$aitable."'");
+						$db->query();
+						$dbresult = $db->loadAssoc();
+						$ai = $dbresult['Auto_increment'];
+						$db->setQuery("SHOW TABLE STATUS LIKE '".$pfx."content'");
+						$db->query();
+						$dbresult = $db->loadAssoc();
+						$content_ai = $dbresult['Auto_increment'];
+						$inifile = dirname(__FILE__).DS.'increments.ini';
+						if(JFile::exists($inifile)){
+							chmod($inifile, 0644);
+							$ini = parse_ini_file($inifile); //get ini values as array
+							if(array_key_exists($sebid, $ini)){
+								if($ini[$sebid] > '' && !$ini[$sebid] == 0){
+									$ai = $ini[$sebid];
+								}else{
+									$ini_in = "";
+									if(!is_array($ini)){
+										$ini = array();
+									}
+									$ini[$sebid] = $ai; //$ai for next auto increment
+									foreach($ini as $key=>$value){
+										$ini_in .= $key."=".$value."\r\n";
+									}
+									$fopen = fopen($inifile, 'w');
+									fwrite($fopen, $ini_in);
+									fclose($fopen);	
+								}
+							}else{
+								$ini_in = "";
+								$ini[$sebid] = $ai; //$ai for next auto increment
+								foreach($ini as $key=>$value){
+									$ini_in .= $key."=".$value."\r\n";
+								}
+								$fopen = fopen($inifile, 'w');
+								fwrite($fopen, $ini_in);
+								fclose($fopen);	
+							}
+						};
+						
+					}else{
+						$ai = "";
+					};
+				};
+				
+				$sdField = $ai;
+				($sdField > '' ? $sdConcatValue .= $sdField.($sdNumStep < $sdNumFields ? $sdFieldSeparator : null) : null);
+				/* End Next Auto Increment */
+				
+				elseif(preg_match("^[\[\]]^", $sdField)):
 					$sdField = str_replace(array('[',']'), array('',''), $sdField);
+					$sdField = ($SdJtext == 1 ? JText::_('PLG_CCK_FIELD_SD_FIELD_CONCAT_'.str_replace(array(';',':','`','~','}','{','>','<','!','=',',','-'),'',trim(strtoupper(str_replace(' ', '_', $sdField))))) : $sdField);
 					($sdField > '' ? $sdConcatValue .= $sdField.($sdNumStep < $sdNumFields ? $sdFieldSeparator : null) : null);
 				elseif(preg_match("^[\$]^", $sdField)):
 					$sdFind = array('$date', '$time', '$username', '$userid');
-					$sdReplace = array(date('Y-m-d'), date('H:m:s'), ($user->username > '' ? $user->username : ''), ($user->id > '' ? $user->id : ''));
+					$sdReplace = array(date($SdDateFormat), date('H:m:s'), ($user->username > '' ? $user->username : ''), ($user->id > '' ? $user->id : ''));
 					$sdField = str_replace($sdFind, $sdReplace, $sdField);
 					($sdField > '' ? $sdConcatValue .= $sdField.($sdNumStep < $sdNumFields ? $sdFieldSeparator : null) : null);
 				elseif(preg_match("^[#(.*)#]^", $sdField)):
 					$sdField = trim(str_replace('#','',$sdField));
-					($sdField > '' ? $sdConcatValue .= trim($config['post'][$sdField]).($sdNumStep < $sdNumFields ? $sdFieldSeparator : null) : null);
+					$sdFieldTmp = CCK_Field::getObject($sdField);
+					if($sdFieldTmp->type == 'select_simple' || $sdFieldTmp->type == 'select_dynamic' || $sdFieldTmp->type == 'radio' || $sdFieldTmp->type == 'checkbox' || $sdFieldTmp->type == 'select_dynamic_cascade'){
+							switch($sdFieldTextValue){
+								case 't':
+									$sdFieldTmp = parent::g_getOptionText( trim($config['post'][$sdField]), $sdFieldTmp->options, '', $config );
+									$sdConcatValue .= $sdFieldTmp;
+									$sdFieldTmp = '';
+								break;
+								
+								case 'v':
+									($sdField > '' ? $sdConcatValue .= trim($config['post'][$sdField]).($sdNumStep < $sdNumFields ? $sdFieldSeparator : null) : null);
+								break;
+								
+								default:
+									($sdField > '' ? $sdConcatValue .= trim($config['post'][$sdField]).($sdNumStep < $sdNumFields ? $sdFieldSeparator : null) : null);
+								break;
+							};
+					}else{
+						($sdField > '' ? $sdConcatValue .= trim($config['post'][$sdField]).($sdNumStep < $sdNumFields ? $sdFieldSeparator : null) : null);
+					};				
 				endif;
 			endforeach;
 
 		else:
-			null;
+			/* SINGLE FIELDS*/
+				$sdConcatValue = '';
+				$sdNumFields = 1;
+				$sdNumStep = 0;
+				$sdField = trim($sdFieldList);
+				if(preg_match("^<(.*?)>^", $sdField, $matches)):
+				$aitable = $matches[1];
+				/* Next Auto Increment */
+				$sebid = $config['pk'];
+				if(!$sebid > 0){	// Are we creating a new article? Yes
+				echo "we are add new, ";
+					$inifile = dirname(__FILE__).DS.'increments.ini';
+					if(JFile::exists($inifile)){
+						chmod($inifile, 0644);
+							$ini = parse_ini_file($inifile);
+							$db = JFactory::getDbo();
+							$pfx = $db->getPrefix();
+							// get auto increment of users desired table
+							$db->setQuery("SHOW TABLE STATUS LIKE '".$pfx.$aitable."'");
+							$db->query();
+							$dbresult = $db->loadAssoc();
+							$ai = $dbresult['Auto_increment'];
+							// get auto increment of content table for this ID
+							$db->setQuery("SHOW TABLE STATUS LIKE '".$pfx."content'");
+							$db->query();
+							$dbresult = $db->loadAssoc();
+							$artid = $dbresult['Auto_increment'];
+							$ini[$artid] = $ai;
+							$fopen = fopen($inifile, 'w');
+							$ini_in = "";
+							foreach($ini as $key=>$val){
+								$ini_in .= $key."=".$val."\r\n";
+							}
+							fwrite($fopen, $ini_in);
+							fclose($fopen);						
+					};
+				}else{ // We are updating an article
+					if($aitable > ''){
+						$sebid = $config['pk'];
+						$db = JFactory::getDbo();
+						$pfx = $db->getPrefix();
+						$db->setQuery("SHOW TABLE STATUS LIKE '".$pfx.$aitable."'");
+						$db->query();
+						$dbresult = $db->loadAssoc();
+						$ai = $dbresult['Auto_increment'];
+						$db->setQuery("SHOW TABLE STATUS LIKE '".$pfx."content'");
+						$db->query();
+						$dbresult = $db->loadAssoc();
+						$content_ai = $dbresult['Auto_increment'];
+						$inifile = dirname(__FILE__).DS.'increments.ini';
+						if(JFile::exists($inifile)){
+							chmod($inifile, 0644);
+							$ini = parse_ini_file($inifile); //get ini values as array
+							if(array_key_exists($sebid, $ini)){
+								if($ini[$sebid] > '' && !$ini[$sebid] == 0){
+									$ai = $ini[$sebid];
+								}else{
+									$ini_in = "";
+									if(!is_array($ini)){
+										$ini = array();
+									}
+									$ini[$sebid] = $ai; //$ai for next auto increment
+									foreach($ini as $key=>$value){
+										$ini_in .= $key."=".$value."\r\n";
+									}
+									$fopen = fopen($inifile, 'w');
+									fwrite($fopen, $ini_in);
+									fclose($fopen);	
+								}
+							}else{
+								$ini_in = "";
+								$ini[$sebid] = $ai; //$ai for next auto increment
+								foreach($ini as $key=>$value){
+									$ini_in .= $key."=".$value."\r\n";
+								}
+								$fopen = fopen($inifile, 'w');
+								fwrite($fopen, $ini_in);
+								fclose($fopen);	
+							}
+						};
+						
+					}else{
+						$ai = "";
+					};
+				};
+				
+				$sdField = $ai;
+				($sdField > '' ? $sdConcatValue .= $sdField.($sdNumStep < $sdNumFields ? $sdFieldSeparator : null) : null);
+				/* End Next Auto Increment */
+				elseif(preg_match("^[\[\]]^",$sdField )):
+					$sdField = str_replace(array('[',']'), array('',''), $sdField );
+					$sdField = ($SdJtext == 1 ? JText::_('PLG_CCK_FIELD_SD_FIELD_CONCAT_'.str_replace(array(';',':','`','~','}','{','>','<','!','=',',','-'),'',trim(strtoupper(str_replace(' ', '_', $sdField))))) : $sdField);
+					($sdField > '' ? $sdConcatValue .= $sdField.($sdNumStep < $sdNumFields ? $sdFieldSeparator : null) : null);
+				elseif(preg_match("^[\$]^", $sdField)):
+					$sdFind = array('$date', '$time', '$username', '$userid');
+					$sdReplace = array(date($SdDateFormat), date('H:m:s'), ($user->username > '' ? $user->username : ''), ($user->id > '' ? $user->id : ''));
+					$sdField = str_replace($sdFind, $sdReplace, $sdField);
+					($sdField > '' ? $sdConcatValue .= $sdField.($sdNumStep < $sdNumFields ? $sdFieldSeparator : null) : null);
+				elseif(preg_match("^[#(.*)#]^", $sdField)):
+					$sdField = trim(str_replace('#','',$sdField));
+					$sdFieldTmp = CCK_Field::getObject($sdField);
+					if($sdFieldTmp->type == 'select_simple' || $sdFieldTmp->type == 'select_dynamic' || $sdFieldTmp->type == 'radio' || $sdFieldTmp->type == 'checkbox' || $sdFieldTmp->type == 'select_dynamic_cascade'){
+							switch($sdFieldTextValue){
+								case 't':
+									$sdFieldTmp = parent::g_getOptionText( trim($config['post'][$sdField]), $sdFieldTmp->options, '', $config );
+									$sdConcatValue .= $sdFieldTmp;
+									$sdFieldTmp = '';
+								break;
+								
+								case 'v':
+									($sdField > '' ? $sdConcatValue .= trim($config['post'][$sdField]).($sdNumStep < $sdNumFields ? $sdFieldSeparator : null) : null);
+								break;
+								
+								default:
+									($sdField > '' ? $sdConcatValue .= trim($config['post'][$sdField]).($sdNumStep < $sdNumFields ? $sdFieldSeparator : null) : null);
+								break;
+							};
+					}else{
+						($sdField > '' ? $sdConcatValue .= trim($config['post'][$sdField]).($sdNumStep < $sdNumFields ? $sdFieldSeparator : null) : null);
+					};				
+				endif;
+				/* END SINGLE FEILD */
 		endif;
-
 		$value = $sdConcatValue;
 		
 		##############################################################
@@ -154,13 +401,39 @@ class plgCCK_FieldSd_Field_Concat extends plgCCK_FieldGeneric
 	// onCCK_FieldRenderContent
 	public static function onCCK_FieldRenderContent( $field, &$config = array() )
 	{
-		return parent::g_onCCK_FieldRenderContent( $field, 'html' );
+		return parent::g_onCCK_FieldRenderContent( $field );
 	}
 	
 	// onCCK_FieldRenderForm
 	public static function onCCK_FieldRenderForm( $field, &$config = array() )
 	{		
 		return parent::g_onCCK_FieldRenderForm( $field );
+	}
+	
+	// onCCK_FieldBeforeStore
+	public static function onCCK_FieldBeforeStore( $process, &$fields, &$storages, &$config = array() )
+	{
+	}
+	
+	// onCCK_FieldAfterStore
+	public static function onCCK_FieldAfterStore( $process, &$fields, &$storages, &$config = array() )
+	{
+	}
+	
+	// onCCK_FieldPrepareSearch
+	public function onCCK_FieldPrepareSearch( &$field, $value = '', &$config = array(), $inherit = array(), $return = false )
+	{
+		if ( self::$type != $field->type ) {
+			return;
+		}
+		
+		// Prepare
+		self::onCCK_FieldPrepareForm( $field, $value, $config, $inherit, $return );
+		
+		// Return
+		if ( $return === true ) {
+			return $field;
+		}
 	}
 
 }
--- tmpl/edit.php
+++ tmpl/edit.php
@@ -17,11 +17,20 @@ $options2	=	CCK_Dev::fromJSON( $this->item->options2 );
 ?>
 
 <div class="seblod">
+	<?php
+	$SdConcatDonate = "<div style=\"overflow:hidden;clear:both;padding:10px;margin:0 0 15px 0;border:1px dotted #666;background:#e1ebf2;\"><h6 style=\"font-size:14px;padding:0;margin:0 0 15px 0;color:#444;text-shadow:1px 1px 1px #fff;\">PLEASE SUPPORT FURTHER DEVELOPMENT BY DONATING SOMETHING SMALL</h6>";
+	$SdConcatDonate.= '<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=TCGRYXG5Y8GY6&lc=ZA&item_name=Simon%20Dowdles&item_number=sd_field_concat&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donate_LG%2egif%3aNonHosted" title="Donate Something" target="_blank" style="display:block;float:left;clear:both;padding:10px 12px;background:#4a98d3;color:#fff;text-align:center;font-weight:bold;-moz-border-radius:8px;-webkit-border-radius:8px;border-radius:8px;text-shadow:0 0 1px #2d80c1;font-size:12px;">Donate Via PayPal</a>';
+	$SdConcatDonate.= "</div>";
+	echo $SdConcatDonate;
+	?>
 	<?php echo CCK_Dev::renderLegend( JText::_( 'COM_CCK_CONSTRUCTION' ), JText::_( 'PLG_CCK_FIELD_'.$this->item->type.'_DESC' ) ); ?>
-    <ul class="adminformlist adminformlist-2cols">
+	<ul class="adminformlist adminformlist-2cols">
 		<?php
 		echo CCK_Dev::renderForm('sd_core_field_concat_list', @$options2['sd_core_field_concat_list'], $config);
 		echo CCK_Dev::renderForm('sd_core_field_concat_separator', @$options2['sd_core_field_concat_separator'], $config);
+		echo CCK_Dev::renderForm('sd_core_field_concat_date_format', @$options2['sd_core_field_concat_date_format'], $config);
+		echo CCK_Dev::renderForm('sd_core_field_concat_enable_jtext', @$options2['sd_core_field_concat_enable_jtext'], $config);
+		echo CCK_Dev::renderForm('sd_core_field_concat_text_value', @$options2['sd_core_field_concat_text_value'], $config);
 		echo CCK_Dev::renderSpacer( JText::_( 'COM_CCK_STORAGE' ), JText::_( 'COM_CCK_STORAGE_DESC' ) );
 		echo CCK_Dev::getForm( 'core_storage', $this->item->storage, $config );
         ?>
